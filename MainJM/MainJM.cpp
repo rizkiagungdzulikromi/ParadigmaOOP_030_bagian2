3d7b1d5ee573430926637a96802efa5d560c1e2d
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2023-05-24 20:28:59 +0700

    bang bang susah bang

---
+++ MainJM/MainJM.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "Jantung.h"
+#include "Manusia.h"
+
+
+int main() {
+	manusia* varManusia = new manusia("jono");
+	delete varManusia;
+	return 0;
+}
